37ea3c4e46e1ad64a1cd16515fd180b96e1d02a0
Kishkuman13 <amos.martineau@gmail.com>
2021-02-24 21:02:57 -0700

    Created Login component

---
+++ water-my-plants/src/components/LoginForm.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+
+export default function LogInForm(props) {
+  const {
+    logInData,
+    inputChange,
+    formSubmit
+  } = props
+  
+  const onSubmit = evt => {
+    evt.preventDefault()
+    formSubmit()
+  }
+
+  const onChange = evt => {
+    const { name, value } = evt.target
+    inputChange(name, value)
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>Log In</h1>
+      </header>
+      <section>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label>Username
+              <input
+                value={logInData.username}
+                onChange={onChange}
+                name='username'
+                type='text'
+                placeholder='Username'
+              />
+            </label>
+            <label>Password
+              <input
+                value={logInData.password}
+                onChange={onChange}
+                name='password'
+                type='text'
+                placeholder='Password'
+              />
+            </label>
+          </div>
+          <button>
+            Log In
+          </button>
+        </form>
+        <h3>or</h3>
+        <Router>
+          <button>Sign Up
+            <Link to='/signup'/>
+          </button>
+        </Router>
+      </section>
+    </div>
+  )
+}
