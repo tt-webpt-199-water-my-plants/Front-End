dddcd4efd2bb23645581b46a1fd17a2ac384ff88
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-02-26 22:11:50 -0500

    Add plants list

--- water-my-plants/src/components/Plant.js
+++ water-my-plants/src/components/Plant.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Plant = () => {
+const Plant = (props) => {
+    const { plant } = props;
+    const plantImage = (plant.image === 'null' || !plant.image) ? '/plant-thumbnail.png' : plant.image;
     return ( 
         <div>
-            
+            <img src={plantImage} />
+            <div className="plant-content">
+                <p>{plant.nickname} <span>{plant.speciesName}</span></p>
+                <p>Watering Frequency: {plant.h20Frequency}</p>
+            </div>
+            <button>x</button>
         </div>
      );
 }
--- water-my-plants/src/components/PlantsList.js
+++ water-my-plants/src/components/PlantsList.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import Plant from './Plant';
 
 const PlantsList = () => {
+    const [plants, setPlants] = useState([]);
+
+    const getPlants = () => {
+        axiosWithAuth().get('https://water-my-plants-api-t199.herokuapp.com/api/plants')
+        .then((response) => {
+            setPlants(response.data)
+        });
+    }
+
+    useEffect(() => {
+        getPlants();
+    }, []);
+
     return ( 
         <div>
-            
+            {
+                plants.map((plant) => {
+                    return <Plant plant={plant} key={plant.id} />
+                })
+            }
         </div>
      );
 }
