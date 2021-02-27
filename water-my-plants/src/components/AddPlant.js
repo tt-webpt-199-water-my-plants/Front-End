6388d6beb1a0971d1ac4c0ac733ca8d3021ebdd5
ngwatso <ngwatson79@yahoo.com>
2021-02-27 18:51:31 -0500

    added image upload to AddPlant

--- water-my-plants/src/components/AddPlant.js
+++ water-my-plants/src/components/AddPlant.js
@@ -1,82 +1,143 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { connect } from 'react-redux';
 import { addNewPlant } from '../actions';
 
-class AddPlant extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			id: Date.now(),
-			nickname: '',
-			h2OFrequency: '',
-			speciesName: '',
-			image: '',
-		};
-	}
-
-	handleChange = (e) => {
-		this.setState({ ...this.state, [e.target.name]: e.target.value });
+const AddPlant = (props, { plants, isLoading, error }) => {
+	const initialState = {
+		id: Date.now(),
+		nickname: '',
+		h2OFrequency: '',
+		speciesName: '',
+		image: null,
 	};
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.props.addNewPlant(this.state);
+	const [state, setState] = useState(initialState);
+	// class AddPlant extends React.Component {
+	// 	constructor() {
+	// 		super();
+	// 		this.state = {
+	// 			id: Date.now(),
+	// 			nickname: '',
+	// 			h2OFrequency: '',
+	// 			speciesName: '',
+	// 			image: null,
+	// 		};
+	// 	}
 
-		this.setState({
-			id: Date.now(),
+	const handleChange = (e) => {
+		setState({ ...state, [e.target.name]: e.target.value });
+	};
+
+	const uploadedImage = useRef(null);
+	const imageUploader = useRef(null);
+
+	const handleImageUpload = (e) => {
+		const [file] = e.target.files;
+		if (file) {
+			const reader = new FileReader();
+			const { current } = uploadedImage;
+			current.file = file;
+			reader.onload = (e) => {
+				current.src = e.target.result;
+			};
+			reader.readAsDataURL(file);
+		}
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		props.addNewPlant(state);
+
+		setState({
+			id: '',
 			nickname: '',
 			h2OFrequency: '',
 			speciesName: '',
-			image: '',
+			image: null,
 		});
 	};
 
-	render() {
-		return (
-			<section>
-				<h2>Add Plant</h2>
-				<form onSubmit={this.handleSubmit}>
-					<div className="newPlant">
+	return (
+		<section>
+			<h2>Add Plant</h2>
+			<form onSubmit={handleSubmit}>
+				<div className="newPlant">
+					<input
+						value={state.nickname}
+						onChange={handleChange}
+						name="nickname"
+						id="nickname"
+						placeholder="Nickname"
+					/>
+					<input
+						value={state.h2OFrequency}
+						onChange={handleChange}
+						name="h2OFrequency"
+						id="h2OFrequency"
+						placeholder="H2O Frequency"
+					/>
+					<input
+						value={state.speciesName}
+						onChange={handleChange}
+						name="speciesName"
+						id="speciesName"
+						placeholder="Species Name"
+					/>
+					<div
+						className="img-button"
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+					>
 						<input
-							value={this.state.nickname}
-							onChange={this.handleChange}
-							name="nickname"
-							id="nickname"
-							placeholder="Nickname"
+							type="file"
+							accept="image/*"
+							// multiple={false}
+							// value={state.image}
+							onChange={handleImageUpload}
+							ref={imageUploader}
+							style={{
+								display: 'none',
+							}}
+							// name="image"
+							// id="image"
 						/>
-						<input
-							value={this.state.h2OFrequency}
-							onChange={this.handleChange}
-							name="h2OFrequency"
-							id="h2OFrequency"
-							placeholder="H2O Frequency"
-						/>
-						<input
-							value={this.state.speciesName}
-							onChange={this.handleChange}
-							name="speciesName"
-							id="speciesName"
-							placeholder="Species Name"
-						/>
-						<input
-							value={this.state.image}
-							onChange={this.handleChange}
-							name="image"
-							id="image"
-							placeholder="Image URL"
-						/>
+						<div
+							className="preview-img"
+							style={{
+								height: '60px',
+								width: '60px',
+								border: '1px dashed black',
+							}}
+							onClick={() => imageUploader.current.click()}
+						>
+							<img
+								ref={uploadedImage}
+								style={{
+									width: '100%',
+									height: '100%',
+									maxWidth: '60px',
+									maxHeight: '60px',
+									position: 'absolute',
+								}}
+								alt="flower"
+							/>
+						</div>
 					</div>
-					<button>Submit Plant</button>
-				</form>
-			</section>
-		);
-	}
-}
+					Click to upload Image
+				</div>
+			</form>
+		</section>
+	);
+};
 
 const mapStateToProps = (state) => {
 	return {
 		plants: state.plants,
-		isLoading: state.inLoading,
+		isLoading: state.isLoading,
 		error: state.error,
 	};
 };
