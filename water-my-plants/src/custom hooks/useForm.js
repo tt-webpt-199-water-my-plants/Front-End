722242c78acb84e771e84067f56cacdcff2e3ab5
ngwatso <ngwatson79@yahoo.com>
2021-02-28 20:08:01 -0500

    created custom hooks to handle session storage

---
+++ water-my-plants/src/custom hooks/useForm.js
@@ -0,0 +1,21 @@
+import useLocalStorage from './sessionStorage';
+
+const useForm = (initialValue) => {
+	const [values, setValues] = useLocalStorage('form', initialValues);
+
+	const handleChanges = (e) => {
+		setValues({
+			...values,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const clearForm = (e) => {
+		e.preventDefault();
+		setValues(initialValue);
+	};
+
+	return [values, handleChanges, clearForm];
+};
+
+export default useForm;
