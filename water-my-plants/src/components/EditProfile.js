2c73092ea2a38252a2094deb2a8b1b57702da64a
ngwatso <ngwatson79@yahoo.com>
2021-03-05 19:46:57 -0500

    removed unneeded dependancies

--- water-my-plants/src/components/EditProfile.js
+++ water-my-plants/src/components/EditProfile.js
@@ -6,7 +6,6 @@ import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import * as yup from 'yup';
 import FormSchemaEditProfile from '../validation/FormSchemaEditProfile';
-import { Base64 } from 'js-base64';
 
 const StyledEditProfile = styled.div`
 	text-align: center;
